1a1db504a38292494d6a0661dacbe80f93c3e7b3
Timurla <79695583+Timurla@users.noreply.github.com>
2021-02-26 13:21:54 +0300

    Add files via upload

---
+++ Марафон/Марафон/Form3.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Марафон
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(22, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Назад";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(126, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(388, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MARATHON SKILLS 2016";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(309, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(244, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Подробная информация";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 14.25F);
+            this.button2.Location = new System.Drawing.Point(183, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(193, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Marathon skills 2016";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Arial", 14.25F);
+            this.button3.Location = new System.Drawing.Point(183, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(193, 37);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Предыдущие результаты ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Arial", 14.25F);
+            this.button4.Location = new System.Drawing.Point(183, 266);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(193, 33);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "BMI Калькулятор";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Arial", 14.25F);
+            this.button5.Location = new System.Drawing.Point(446, 142);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(260, 35);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Насколько долгий марафон ";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Arial", 14.25F);
+            this.button6.Location = new System.Drawing.Point(446, 192);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(260, 54);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Список благотворительных организацияй";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Arial", 14.25F);
+            this.button7.Location = new System.Drawing.Point(446, 266);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(260, 36);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "BMR калькулятор";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form3";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
